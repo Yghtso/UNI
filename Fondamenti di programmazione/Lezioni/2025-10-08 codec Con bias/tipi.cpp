ade1840e9fe727f0459f5a450c1a69f7caef9bed
Cristian Soare <soare.cristian@outlook.com>
2025-10-08 11:26:17 +0000

    vault backup: 2025-10-08 11:26:17

---
+++ Fondamenti di programmazione/Lezioni/2025-10-08 codec Con bias/tipi.cpp
@@ -0,0 +1,14 @@
+
+int main (int argc, char *argv[]) {
+    
+    int i;          // 4 byte
+
+    // se non specifico il tipo di dato, è int
+    
+    short i;        // 2 byte
+    long i;         // dipende dal sistema 4/8 byte
+    long long i;    // 8 byte
+    unsigned i;     // stessi bit (del dato primitivo)
+
+    return 0;
+}
